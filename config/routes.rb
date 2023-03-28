cc741fb7fc38f9fa12f828c3ea8f79a87eafeb45
Chazz Gagliano <cgags17285@gmail.com>
2023-03-28 00:31:14 -0400

    controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,11 @@ Rails.application.routes.draw do
   #httpie                    #controller               #method
   get"/hello", controller: "samples", action: "hello_there"
 end
+
+
+
+class SamplesController < ApplicationController
+  def hello_there
+    render json: "hello"
+  end
+end
